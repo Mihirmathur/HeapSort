22d839fb2b9ce9d32f12cf3b0bd6f10d50ab7a59
Melody Spencer <Melody Spencer>
2016-09-17 15:14:51 -0400

    ui changes

--- HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
+++ HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
@@ -47,48 +47,49 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             thumbnail.setImageBitmap(imageBitmap);
+            clickme.setText("");
         }
     }
 
-    String trashCategories() {
-
-        String[] recycle = {"bottle", "can"};
-        String[] compost = {"food"};
-        String[] landfill = ["", ""];
-
-        int recycleCount = 0;
-        int compostCount = 0;
-        int landfillCount = 0;
-
-        String[] tagNames;
-        int[] tagScores =;
-
-        //put tag names in tagNames
-        for (int i = 0; i < tags[].Name.size(); i++) {
-            tagNames[i] = tags[i].Name;
-        }
-
-        //put tag scores in tagScores
-        for (int i = 0; i < tags[].Score.size(); i++) {
-            tagScores[i] = tags[i].Score;
-        }
-
-        for (int i = 0; i < tagNames.size(); i++) {
-            if (tagNames[i] == recycle[i]) {
-                recycleCount++;
-            } else if (tagNames[i] == compost[i]) {
-                compostCount++;
-            } else if (tagNames[i] == landfill[i]) {
-                landfillCount++;
-            }
-        }
-
-        if (recycleCount > compostCount && recycleCount > landfillCount) {
-            return "Please throw into the RECYCLE bin.";
-        } else if (compostCount > recycleCount && compostCount > landfillCount) {
-            return "Please throw into the COMPOST bin.";
-        } else
-            return "Please throw into the LANDFILL bin.";
-    }
+//    String trashCategories() {
+//
+//        String[] recycle = {"bottle", "can"};
+//        String[] compost = {"food"};
+//        String[] landfill = {"", ""};
+//
+//        int recycleCount = 0;
+//        int compostCount = 0;
+//        int landfillCount = 0;
+//
+//        String[] tagNames;
+//        int[] tagScores;
+//
+//        //put tag names in tagNames
+//        for (int i = 0; i < tags[].Name.size(); i++) {
+//            tagNames[i] = tags[i].Name;
+//        }
+//
+//        //put tag scores in tagScores
+//        for (int i = 0; i < tags[].Score.size(); i++) {
+//            tagScores[i] = tags[i].Score;
+//        }
+//
+//        for (int i = 0; i < tagNames.size(); i++) {
+//            if (tagNames[i] == recycle[i]) {
+//                recycleCount++;
+//            } else if (tagNames[i] == compost[i]) {
+//                compostCount++;
+//            } else if (tagNames[i] == landfill[i]) {
+//                landfillCount++;
+//            }
+//        }
+//
+//        if (recycleCount > compostCount && recycleCount > landfillCount) {
+//            return "Please throw into the RECYCLE bin.";
+//        } else if (compostCount > recycleCount && compostCount > landfillCount) {
+//            return "Please throw into the COMPOST bin.";
+//        } else
+//            return "Please throw into the LANDFILL bin.";
+//    }
 
 }
