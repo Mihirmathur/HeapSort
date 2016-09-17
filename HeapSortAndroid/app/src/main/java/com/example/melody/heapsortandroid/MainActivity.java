f74e5e255f3c235d2474cd99a8d5ded44e99596b
Melody Spencer <Melody Spencer>
2016-09-17 14:20:32 -0400

    Added Click Button and basic UI

--- HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
+++ HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.melody.heapsortandroid;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.Image;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
@@ -9,14 +10,22 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView thumbnail;
+    Button clickme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        thumbnail = (ImageView) findViewById(R.id.thumbnail);
+        clickme = (Button) findViewById(R.id.clickme);  
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -28,16 +37,16 @@ public class MainActivity extends AppCompatActivity {
                     startActivityForResult(takePictureIntent, 1);
                 }
             }
-
-            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-                if (requestCode == 1 && resultCode == RESULT_OK) {
-                    Bundle extras = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) extras.get("data");
-                }
-            }
-
-            
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            thumbnail.setImageBitmap(imageBitmap);
+        }
     }
 
 }
