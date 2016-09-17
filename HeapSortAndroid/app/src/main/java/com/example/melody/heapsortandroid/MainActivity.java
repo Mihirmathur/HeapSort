aeeaaa58eb48a612b743acc7ad0aba20c2c8d031
Richard Chen <Richard Chen>
2016-09-17 14:27:29 -0400

    added trashCategories

--- HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
+++ HeapSortAndroid/app/src/main/java/com/example/melody/heapsortandroid/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -36,7 +38,48 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
-            
+            String trashCategories() {
+
+                String[] recycle = {"bottle", "can"};
+                String[] compost = {"food"};
+                String[] landfill = ["", ""];
+
+                int recycleCount = 0;
+                int compostCount = 0;
+                int landfillCount = 0;
+
+                String[] tagNames;
+                int[] tagScores =;
+
+                //put tag names in tagNames
+                for (int i = 0; i < tags[].Name.size(); i++) {
+                    tagNames[i] = tags[i].Name;
+                }
+
+                //put tag scores in tagScores
+                for (int i = 0; i < tags[].Score.size(); i++) {
+                    tagScores[i] = tags[i].Score;
+                }
+
+                for (int i = 0; i < tagNames.size(); i++) {
+                    if (tagNames[i] == recycle[i]) {
+                        recycleCount++;
+                    } else if (tagNames[i] == compost[i]) {
+                        compostCount++;
+                    } else if (tagNames[i] == landfill[i]) {
+                        landfillCount++;
+                    }
+                }
+
+                if (recycleCount > compostCount && recycleCount > landfillCount) {
+                    return "Please throw into the RECYCLE bin.";
+                } else if (compostCount > recycleCount && compostCount > landfillCount) {
+                    return "Please throw into the COMPOST bin.";
+                } else
+                    return "Please throw into the LANDFILL bin.";
+            }
+
+
         });
     }
 
